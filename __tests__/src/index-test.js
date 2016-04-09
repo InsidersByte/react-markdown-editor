569e6e6e2463d1ff150966a3385a58c5f8b584b1
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-04-09 10:22:21 +0100

    refactor tests

--- __tests__/src/index-test.js
+++ __tests__/src/index-test.js
@@ -11,21 +11,37 @@ const value = '';
 const onChangeMock = jest.genMockFunction();
 const onImageDropMock = jest.genMockFunction();
 
+let markdownEditor;
+
 const validFile = [{ type: 'image/jpeg', name: 'image' }];
 const invalidFile = [{ type: 'text/markdown', name: 'markdown' }];
 const uploadedImage = { url: 'url', filename: 'image' };
 
+function generateMarkdownEditor(includeOnDropImage = false) {
+    const markup = includeOnDropImage ?
+        <MarkdownEditor value={value} onChange={onChangeMock} onImageDrop={onImageDropMock} /> :
+        <MarkdownEditor value={value} onChange={onChangeMock} />;
+
+    return TestUtils.renderIntoDocument(
+        markup
+    );
+}
+
+function simulateDrop(files, useDataTransfer = true) {
+    TestUtils.Simulate.drop(
+        TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
+        { [useDataTransfer ? 'dataTransfer' : 'target']: { files } }
+    );
+}
+
 describe('MarkdownEditor', () => {
     beforeEach(() => {
+        markdownEditor = generateMarkdownEditor();
         onChangeMock.mockClear();
         onImageDropMock.mockClear();
     });
 
     it('renders', () => {
-        const markdownEditor = TestUtils.renderIntoDocument(
-            <MarkdownEditor value={value} onChange={onChangeMock} />
-        );
-
         const markdownEditorNode = ReactDOM.findDOMNode(markdownEditor);
 
         expect(markdownEditorNode).toBeDefined();
@@ -36,15 +52,9 @@ describe('MarkdownEditor', () => {
 
     describe('onDragEnter', () => {
         it('sets draggingOver to true', () => {
-            const markdownEditor = TestUtils.renderIntoDocument(
-                <MarkdownEditor value={value} onChange={onChangeMock} />
-            );
-
             expect(markdownEditor.state.draggingOver).toBe(false);
 
-            TestUtils.Simulate.dragEnter(
-                TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea')
-            );
+            TestUtils.Simulate.dragEnter(TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'));
 
             expect(markdownEditor.state.draggingOver).toBe(true);
         });
@@ -52,15 +62,9 @@ describe('MarkdownEditor', () => {
 
     describe('onDragLeave', () => {
         it('sets draggingOver to false', () => {
-            const markdownEditor = TestUtils.renderIntoDocument(
-                <MarkdownEditor value={value} onChange={onChangeMock} />
-            );
-
             markdownEditor.setState({ draggingOver: true });
 
-            TestUtils.Simulate.dragLeave(
-                TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea')
-            );
+            TestUtils.Simulate.dragLeave(TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'));
 
             expect(markdownEditor.state.draggingOver).toBe(false);
         });
@@ -68,194 +72,97 @@ describe('MarkdownEditor', () => {
 
     describe('onDrop', () => {
         it('sets draggingOver to false', () => {
-            const markdownEditor = TestUtils.renderIntoDocument(
-                <MarkdownEditor value={value} onChange={onChangeMock} />
-            );
-
             markdownEditor.setState({ draggingOver: true });
 
-            TestUtils.Simulate.drop(
-                TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea')
-            );
+            simulateDrop(validFile);
 
             expect(markdownEditor.state.draggingOver).toBe(false);
         });
 
         describe('valid images, but no onImageDrop prop passed', () => {
             it('does not call onChange', () => {
-                const markdownEditor = TestUtils.renderIntoDocument(
-                    <MarkdownEditor value={value} onChange={onChangeMock} />
-                );
-
                 expect(onChangeMock.mock.calls.length).toBe(0);
 
-                TestUtils.Simulate.drop(
-                    TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                    { dataTransfer: { files: validFile } }
-                );
+                simulateDrop(validFile);
 
                 expect(onChangeMock.mock.calls.length).toBe(0);
             });
         });
 
         describe('onImageDrop prop passed', () => {
-            describe('no files passed', () => {
-                it('does not call onChange or onImageDrop', () => {
-                    const markdownEditor = TestUtils.renderIntoDocument(
-                        <MarkdownEditor
-                            value={value}
-                            onChange={onChangeMock}
-                            onImageDrop={onImageDropMock}
-                        />
-                    );
+            beforeEach(() => {
+                markdownEditor = generateMarkdownEditor(true);
 
-                    expect(onChangeMock.mock.calls.length).toBe(0);
-                    expect(onImageDropMock.mock.calls.length).toBe(0);
+                expect(onChangeMock.mock.calls.length).toBe(0);
+                expect(onImageDropMock.mock.calls.length).toBe(0);
+            });
 
-                    TestUtils.Simulate.drop(
-                        TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                        { dataTransfer: { files: [] } }
-                    );
-
+            describe('invalid files', () => {
+                afterEach(() => {
                     expect(onChangeMock.mock.calls.length).toBe(0);
                     expect(onImageDropMock.mock.calls.length).toBe(0);
                 });
-            });
 
-            describe('no images passed', () => {
-                it('does not call onChange or onImageDrop', () => {
-                    const markdownEditor = TestUtils.renderIntoDocument(
-                        <MarkdownEditor
-                            value={value}
-                            onChange={onChangeMock}
-                            onImageDrop={onImageDropMock}
-                        />
-                    );
+                describe('no files passed', () => {
+                    it('does not call onChange or onImageDrop', () => {
+                        simulateDrop([]);
+                    });
+                });
 
-                    expect(onChangeMock.mock.calls.length).toBe(0);
-                    expect(onImageDropMock.mock.calls.length).toBe(0);
-
-                    TestUtils.Simulate.drop(
-                        TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                        { dataTransfer: { files: invalidFile } }
-                    );
-
-                    expect(onChangeMock.mock.calls.length).toBe(0);
-                    expect(onImageDropMock.mock.calls.length).toBe(0);
+                describe('no images passed', () => {
+                    it('does not call onChange or onImageDrop', () => {
+                        simulateDrop(invalidFile);
+                    });
                 });
             });
 
             describe('invalid response from onImageDrop', () => {
+                afterEach(() => {
+                    expect(onChangeMock.mock.calls.length).toBe(1);
+                    expect(onChangeMock).toBeCalledWith({ target: { value: '\n![uploading image...]()' } });
+
+                    expect(onImageDropMock.mock.calls.length).toBe(1);
+                    expect(onImageDropMock).toBeCalledWith(validFile[0]);
+                });
+
                 describe('no filename', () => {
                     it('does not call onChange twice', () => {
-                        const markdownEditor = TestUtils.renderIntoDocument(
-                            <MarkdownEditor
-                                value={value}
-                                onChange={onChangeMock}
-                                onImageDrop={onImageDropMock}
-                            />
-                        );
-
-                        expect(onChangeMock.mock.calls.length).toBe(0);
-                        expect(onImageDropMock.mock.calls.length).toBe(0);
-
                         onImageDropMock.mockReturnValueOnce({ url: 'url' });
 
-                        TestUtils.Simulate.drop(
-                            TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                            { dataTransfer: { files: validFile } }
-                        );
-
-                        expect(onChangeMock.mock.calls.length).toBe(1);
-                        expect(onChangeMock).toBeCalledWith({ target: { value: '\n![uploading image...]()' } });
-
-                        expect(onImageDropMock.mock.calls.length).toBe(1);
-                        expect(onImageDropMock).toBeCalledWith(validFile[0]);
+                        simulateDrop(validFile);
                     });
                 });
 
                 describe('no url', () => {
                     it('does not call onChange twice', () => {
-                        const markdownEditor = TestUtils.renderIntoDocument(
-                            <MarkdownEditor
-                                value={value}
-                                onChange={onChangeMock}
-                                onImageDrop={onImageDropMock}
-                            />
-                        );
-
-                        expect(onChangeMock.mock.calls.length).toBe(0);
-                        expect(onImageDropMock.mock.calls.length).toBe(0);
-
                         onImageDropMock.mockReturnValueOnce({ filename: 'filename' });
 
-                        TestUtils.Simulate.drop(
-                            TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                            { dataTransfer: { files: validFile } }
-                        );
-
-                        expect(onChangeMock.mock.calls.length).toBe(1);
-                        expect(onChangeMock).toBeCalledWith({ target: { value: '\n![uploading image...]()' } });
-
-                        expect(onImageDropMock.mock.calls.length).toBe(1);
-                        expect(onImageDropMock).toBeCalledWith(validFile[0]);
+                        simulateDrop(validFile);
                     });
                 });
             });
 
             describe('one image passed', () => {
-                it('calls onChange and onImageDrop', () => {
-                    const markdownEditor = TestUtils.renderIntoDocument(
-                        <MarkdownEditor
-                            value={value}
-                            onChange={onChangeMock}
-                            onImageDrop={onImageDropMock}
-                        />
-                    );
-
-                    expect(onChangeMock.mock.calls.length).toBe(0);
-                    expect(onImageDropMock.mock.calls.length).toBe(0);
-
-                    onImageDropMock.mockReturnValueOnce(uploadedImage);
-
-                    TestUtils.Simulate.drop(
-                        TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                        { dataTransfer: { files: validFile } }
-                    );
-
+                afterEach(() => {
                     expect(onChangeMock.mock.calls.length).toBe(1);
                     expect(onChangeMock).toBeCalledWith({ target: { value: '\n![uploading image...]()' } });
 
                     expect(onImageDropMock.mock.calls.length).toBe(1);
                     expect(onImageDropMock).toBeCalledWith(validFile[0]);
                 });
-            });
 
-            describe('one image passed with event.target.files', () => {
                 it('calls onChange and onImageDrop', () => {
-                    const markdownEditor = TestUtils.renderIntoDocument(
-                        <MarkdownEditor
-                            value={value}
-                            onChange={onChangeMock}
-                            onImageDrop={onImageDropMock}
-                        />
-                    );
-
-                    expect(onChangeMock.mock.calls.length).toBe(0);
-                    expect(onImageDropMock.mock.calls.length).toBe(0);
-
                     onImageDropMock.mockReturnValueOnce(uploadedImage);
 
-                    TestUtils.Simulate.drop(
-                        TestUtils.findRenderedDOMComponentWithTag(markdownEditor, 'TextArea'),
-                        { target: { files: validFile } }
-                    );
+                    simulateDrop(validFile);
+                });
 
-                    expect(onChangeMock.mock.calls.length).toBe(1);
-                    expect(onChangeMock).toBeCalledWith({ target: { value: '\n![uploading image...]()' } });
+                describe('one image passed with event.target.files', () => {
+                    it('calls onChange and onImageDrop', () => {
+                        onImageDropMock.mockReturnValueOnce(uploadedImage);
 
-                    expect(onImageDropMock.mock.calls.length).toBe(1);
-                    expect(onImageDropMock).toBeCalledWith(validFile[0]);
+                        simulateDrop(validFile, false);
+                    });
                 });
             });
         });
